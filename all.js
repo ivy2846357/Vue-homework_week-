e79a27d22a13d39ba4a170f0ae5ac882889ecd0d
ZAII <ivy2846357@icloud.com>
2022-03-05 00:12:19 +0800

    新增表單驗證功能

--- all.js
+++ all.js
@@ -7,8 +7,49 @@ import {
 
 import bsProductModal from './bsProductMdal.js';
 
+/* 載入 vee-validate 套件 */
+
+// 抓取 vee-validate 元件及功能
+const {
+    defineRule,
+    Form,
+    Field,
+    ErrorMessage,
+    configure
+} = VeeValidate;
+// 抓取 vee-validate 的規則
+const {
+    required,
+    email,
+    min,
+    max
+} = VeeValidateRules;
+// 抓取 vee-validate 的多國語系提示
+const {
+    localize,
+    loadLocaleFromURL
+} = VeeValidateI18n;
+
+// 編輯 VeeValidateRules 的驗證功能
+// 第一個參數為自定義的名稱，第二個參數為 VeeValidateRules 要載入的驗證功能
+defineRule('required', required);
+defineRule('email', email);
+defineRule('min', min);
+defineRule('max', max);
+
+// 載入 vee-validate 提供的 JSON 檔案
+loadLocaleFromURL('./zh_TW.json');
+
+// 這裡用來設定中文語系的提示
+configure({
+    //啟用中文語系
+    generateMessage: localize('zh_TW'),
+    // true 為輸入文字時，就立即進行驗證
+    validateOnInput: true,
+});
+
 /* 建立 Vue 的初始及實體化環境 */
-const app = createApp({
+const app = Vue.createApp({
     data() {
         return {
             // 站點網址
@@ -22,7 +63,16 @@ const app = createApp({
             // 購物車資料存放處
             cartProduct: {},
             // 控制 loading 啟用 / 取消
-            isLoading: ''
+            isLoading: false,
+            form: {
+                user: {
+                    email: '',
+                    name: '',
+                    tel: '',
+                    address: ''
+                },
+                message: ''
+            }
 
         }
     },
@@ -119,10 +169,43 @@ const app = createApp({
                     console.log('商品加入失敗')
                 })
         },
+        // 送出訂單
+        onSubmit() {
+            this.isLoading = true;
+            axios.post(`${this.url}/v2/api/${this.api_path}/order`, {
+                    "data": this.form
+                })
+                .then(res => {
+                    alert('訂單已送出');
+                    // 清空表單資料
+                    this.$refs.form.resetForm();
+                    // 清空購物車資料
+                    this.getCart();
+                    this.isLoading = false;
+                })
+                .catch(err => {
+                    console.log('流程錯誤，訂單沒有送出');
+                })
+        },
+        // 自行驗證訂單資料是否送出
+        getOrderList() {
+            axios.get(`${this.url}/v2/api/${this.api_path}/orders`)
+                .then(res => {
+                    console.log(res);
+                })
+        },
+        // 檢查電話格式
+        isPhone(value) {
+            const phoneNumber = /^(09)[0-9]{8}$/;
+            return phoneNumber.test(value) ? true : '需要正確的電話號碼'
+        }
     },
     // 設定元件資料
     components: {
-        bsProductModal
+        bsProductModal,
+        VForm: Form,
+        VField: Field,
+        ErrorMessage: ErrorMessage,
     },
     mounted() {
         // 網頁開啟即取出商品 / 購物車資料
@@ -130,32 +213,5 @@ const app = createApp({
         this.getCart();
     }
 })
-
-
-/* 載入 vee-validate 套件 */
-
-
-// 載入 vee-validate 所有的驗證規則
-Object.keys(VeeValidateRules).forEach(rule => {
-    if (rule !== 'default') {
-        VeeValidate.defineRule(rule, VeeValidateRules[rule]);
-    }
-});
-
-// 讀取外部的資源( ErrorMessage 內容)
-VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
-
-// Activate the locale
-VeeValidate.configure({
-    generateMessage: VeeValidateI18n.localize('zh_TW'),
-    validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
-});
-
-// 載入 vee-validate 所有的驗證規則
-app.component('VForm', VeeValidate.Form);
-app.component('VField', VeeValidate.Field);
-app.component('ErrorMessage', VeeValidate.ErrorMessage);
-
-
 
 app.mount('#app');
